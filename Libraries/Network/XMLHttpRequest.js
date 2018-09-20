b3672ade3613ed2cf77d8459f68ce238969aa467
sunny <1604587251@qq.com>
2018-09-20 14:26:08 +0800

    for solve ：Invalid response for blob ，when respons is “”

--- Libraries/Network/XMLHttpRequest.js
+++ Libraries/Network/XMLHttpRequest.js
@@ -254,7 +254,12 @@ class XMLHttpRequest extends EventTarget(...XHR_EVENTS) {
         if (typeof this._response === 'object' && this._response) {
           this._cachedResponse = BlobManager.createFromOptions(this._response);
         } else {
-          throw new Error(`Invalid response for blob: ${this._response}`);
+          // for solve ：Invalid response for blob ，when respons is “”
+          if (this._response === '')  {
+            this._cachedResponse = null;
+          } else {
+            throw new Error(`Invalid response for blob: ${this._response}`);
+          }
         }
         break;
 
